88f6c0f3adcd93a5c77f143c19ea5fcc64764d3f
이지영 <abcd23589534@gmail.com>
2024-03-14 17:19:16 +0900

    [FEAT] jenkins deploy test
    Former-commit-id: 32f81ebb6afb599e5768dd2331f243de8d87063e

--- server/hansotbab/src/main/java/com/b209/hansotbab/review/dto/request/ReviewRequestDTO.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/review/dto/request/ReviewRequestDTO.java
@@ -1,3 +1,20 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6f9b688c45f0dc604974904a2bd356228d33acaf37cac88dd0b98eb028cfdd6e
-size 435
+package com.b209.hansotbab.review.dto.request;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class ReviewRequestDTO {
+
+    private Long userId;
+    private Long productId;
+    private String reviewContent;
+
+    @Builder
+    public ReviewRequestDTO(Long userId, Long productId, String reviewContent){
+        this.userId = userId;
+        this.productId = productId;
+        this.reviewContent = reviewContent;
+    }
+
+}
