2a61b7ee9f399be01408714ceb887b29b3ffda46
박성인 <psi7218@naver.com>
2024-03-14 16:35:18 +0900

    [Design] 그리드 뷰 및 필터링 리스트 추가
    Former-commit-id: 5bc56a1d349ff49e0e2fcafa1772c6b787d669fa

--- flutter/hansotbab/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter/hansotbab/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -1,10 +1,3 @@
-//
-//  Generated file. Do not edit.
-//
-
-import FlutterMacOS
-import Foundation
-
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:9ebc54b4d21464b875d1289b3dd5e44cd7e68930dbd6e8c7f954e438eb6ad499
+size 147
