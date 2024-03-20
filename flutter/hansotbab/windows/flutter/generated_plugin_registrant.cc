199aec7333793015e5bf0edc6a676e142803e35f
방소영 <bjo8244@naver.com>
2024-03-20 09:30:08 +0900

    [Chore] Firebase Flutter 연동
    Former-commit-id: 5ba885a82fac734e6f95045d91f53eefa6e7c755

--- flutter/hansotbab/windows/flutter/generated_plugin_registrant.cc
+++ flutter/hansotbab/windows/flutter/generated_plugin_registrant.cc
@@ -1,11 +1,3 @@
-//
-//  Generated file. Do not edit.
-//
-
-// clang-format off
-
-#include "generated_plugin_registrant.h"
-
-
-void RegisterPlugins(flutter::PluginRegistry* registry) {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:775d17d8759081bbe4b3749739c9ad9eb4c36e9d50734cf64eb38e35ed78dfc8
+size 331
