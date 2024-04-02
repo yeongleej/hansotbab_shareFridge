23745f9dbd159e3495ffe2308a6904c7735e95e9
방소영 <bjo8244@naver.com>
2024-04-02 12:57:10 +0900

    [Feat] FcmToken 저장 기능 구현
    1. Fcm토큰과 email 필드 속성으로 갖는 FcmToken entity 작성
    2. Fcm토큰과 email을 프론트에서 DTO로 전달받으면 저장하는 기능 구현
    
    
    Former-commit-id: ac0d6a0f84b7102502fb5a62571c73c0b5453b82

--- server/hansotbab/src/main/java/com/b209/hansotbab/alarm/service/AlarmService.java
+++ server/hansotbab/src/main/java/com/b209/hansotbab/alarm/service/AlarmService.java
@@ -1,8 +1,3 @@
-package com.b209.hansotbab.alarm.service;
-
-import com.b209.hansotbab.alarm.dto.request.AlarmRequestDTO;
-
-public interface AlarmService {
-
-    String sendAlarmByToken(AlarmRequestDTO alarmRequestDTO);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e064a1df1fda8e5a51b2bc928495988e550065e9bb083440410c8ffe97a169c8
+size 334
